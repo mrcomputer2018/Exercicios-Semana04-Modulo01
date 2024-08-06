fc676f30eba7efe9d50fb1c92fcf6829780c7000
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-08-06 19:42:17 -0300

    feat: Add basic arithmetic functions to scripts.js

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+function soma(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+function subtracao(numero1, numero2) {
+  return numero1 - numero2;
+}
+
+function multiplicacao(numero1, numero2) {
+  return numero1 * numero2;
+}
+
+function divisao(numero1, numero2) {
+  return numero1 / numero2;
+}
