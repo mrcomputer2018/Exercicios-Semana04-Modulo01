e624b1028d672455198fc7dbacb0e6fd0276871a
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-08-07 06:03:46 -0300

    chore: Update calculator form with submit and reset buttons

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,17 @@
 function soma(numero1, numero2) {
-  return numero1 + numero2;
+   return numero1 + numero2;
 }
 
 function subtracao(numero1, numero2) {
-  return numero1 - numero2;
+   return numero1 - numero2;
 }
 
 function multiplicacao(numero1, numero2) {
-  return numero1 * numero2;
+   return numero1 * numero2;
 }
 
 function divisao(numero1, numero2) {
-  return numero1 / numero2;
+   return numero1 / numero2;
 }
 
 const form = document.querySelector('form');
@@ -19,4 +19,26 @@ const form = document.querySelector('form');
 const inputNumero1 = document.querySelector('.inputNumero1');
 
 const inputNumero2 = document.querySelector('.inputNumero2');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const numero1 = parseInt(inputNumero1.value);
+    const numero2 = parseInt(inputNumero2.value);
+
+    if (isNaN(numero1) || isNaN(numero2)) {
+        alert('Por favor, digite um número válido.');
+        return
+    }
+
+    const resultadoSoma = soma(numero1, numero2);
+    const resultadoSubtracao = subtracao(numero1, numero2);
+    const resultadoMultiplicacao = multiplicacao(numero1, numero2);
+    const resultadoDivisao = divisao(numero1, numero2);
+
+    console.log('Soma:', resultadoSoma);
+    console.log('Subtração:', resultadoSubtracao);
+    console.log('Multiplicação:', resultadoMultiplicacao);
+    console.log('Divisão:', resultadoDivisao);
+});
 
