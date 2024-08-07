d445f20dfa94175ebccb4d54bf53dff435c06074
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-08-07 05:47:49 -0300

    chore: Update calculator form with error messages for input fields

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,3 +15,8 @@ function divisao(numero1, numero2) {
 }
 
 const form = document.querySelector('form');
+
+const inputNumero1 = document.querySelector('.inputNumero1');
+
+const inputNumero2 = document.querySelector('.inputNumero2');
+
