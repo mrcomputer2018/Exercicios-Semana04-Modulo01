3148835b70cf89089dd4548f43e3f60d2c18c7f1
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-08-12 07:13:12 -0300

    feat: criando funcao gerarResultado - [M1S04] Ex 5

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,6 +16,35 @@ function divisao(numero1, numero2) {
    return numero1 / numero2;
 }
 
+function validarNumero(numero) {
+    if (isNaN(numero)) {
+        return false;
+    }
+    return true;
+}
+
+function gerarResultado(numero1, numero2, operacao) {
+    if (operacao === 1) {
+        alert('O resultado da soma é: ' + soma(numero1, numero2));
+        return soma(numero1, numero2);
+    }
+
+    if (operacao === 2) {
+        alert('O resultado da subtração é: ' + subtracao(numero1, numero2));
+        return subtracao(numero1, numero2);
+    }
+
+    if (operacao === 3) {
+        alert('O resultado da multiplicação é: ' + multiplicacao(numero1, numero2));
+        return multiplicacao(numero1, numero2);
+    }
+
+    if (operacao === 4) {
+        alert('O resultado da divisão é: ' + divisao(numero1, numero2));
+        return divisao(numero1, numero2);
+    }
+ }
+
 const form = document.querySelector('form');
 
 const inputNumero1 = document.querySelector('#inputNumero1');
@@ -26,6 +55,8 @@ const select = document.querySelector('select');
 
 const span = document.querySelector('.spanNumero1');
 
+const span2 = document.querySelector('.spanNumero2');   
+
 //document.getElementById("demo").innerHTML = x + y;
 
 form.addEventListener('submit', function(event) {
@@ -34,41 +65,24 @@ form.addEventListener('submit', function(event) {
     const numero1 = parseInt(inputNumero1.value);
     const numero2 = parseInt(inputNumero2.value);
     const operacao = parseInt(select.value);
-    
-    console.log('select:', select.value);
+
     // validacao dos dados
-    if (isNaN(numero1) || isNaN(numero2)) {
+    if (isNaN(numero1)) {
         span.innerHTML = '<p class="text-danger">Por favor, digite um número válido!!!</p>'
         return
     }
 
     if (isNaN(numero2)) {
+        span2.innerHTML = '<p class="text-danger">Por favor, digite um número válido!!!</p>'
+        return
+    }
+    
+    if (isNaN(numero1) && isNaN(numero2)) {
         span.innerHTML = '<p class="text-danger">Por favor, digite um número válido!!!</p>'
-        return
-    }    
-
-    if (operacao === 1) {
-        const resultado = soma(numero1, numero2);
-        alert('Resultado da soma: ' + resultado);
+        span2.innerHTML = '<p class="text-danger">Por favor, digite um número válido!!!</p>'
         return
     }
 
-    if (operacao === 2) {
-        const resultado = subtracao(numero1, numero2);
-        alert('Resultado da subtração: ' + resultado);
-        return
-    }
-
-    if (operacao === 3) {
-        const resultado = multiplicacao(numero1, numero2);
-        alert('Resultado da multiplicação: ' + resultado);
-        return
-    }
-
-    if (operacao === 4) {
-        const resultado = divisao(numero1, numero2);
-        alert('Resultado da divisão: ' + resultado.toFixed(2));
-        return
-    }
+    gerarResultado(numero1, numero2, operacao);
 });
 
